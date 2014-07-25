a412b973ae732f365068ea696284eeec902b31d6
chen <chen@chendeiMac.local>
2014-07-25 15:07:21 +0800

    UPDATE
    1、调整细节参数

--- BannerMenuForView/ViewController.m
+++ BannerMenuForView/ViewController.m
@@ -18,7 +18,8 @@
 
 - (void)viewDidLoad
 {
-    BannerMenuView *bannerMV = [[BannerMenuView alloc] initWithFrame:CGRectMake(0, 60, isIpad?80:60, isIpad?80:60) menuWidth:isIpad?300:200];
+    //这里由于图片即浮标的长宽限制在75以下，不然会变形，这是图片像数的限制
+    BannerMenuView *bannerMV = [[BannerMenuView alloc] initWithFrame:CGRectMake(0, 60, isIpad?75:60, isIpad?75:60) menuWidth:isIpad?300:200];
     [self.view addSubview:bannerMV];
 }
 
